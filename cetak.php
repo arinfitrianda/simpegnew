bac133d648cc65a6d82f4cfcb583cb67d30427c6
Arin Fitrianda <44077382+arinfitrianda@users.noreply.github.com>
2020-08-01 09:18:46 +0700

    nyobain komentar

--- cetak.php
+++ cetak.php
@@ -5,5 +5,7 @@ echo "<html>
 <body onload='window.print();'>
 <img src=\"http://chart.apis.google.com/chart?chl=$nip&chs=100x100&cht=qr&chld=H%7C0\" align=\"left\" hspace=\"10\">$nama
 </body>
-</html>"
+</html>";
+
+//ini nyobain ajaaaaa
 ?>
